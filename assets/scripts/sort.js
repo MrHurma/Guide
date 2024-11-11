6561e70c9c5d683483bd746b01f600339e0b97ef
MrHurma <belvad208@gmail.com>
2024-11-11 11:40:26 +0300

    sort update

--- assets/scripts/sort.js
+++ assets/scripts/sort.js
@@ -1,85 +1,5 @@
-const search = document.querySelector('.search__input_search')
-var card1 = document.querySelector('#card_1')
-var card2 = document.querySelector('#card_2')
-var card3 = document.querySelector('#card_3')
-var card4 = document.querySelector('#card_4')
-var card5 = document.querySelector('#card_5')
-var card6 = document.querySelector('#card_6')
-var card7 = document.querySelector('#card_7')
-var card8 = document.querySelector('#card_8')
-var text = search.textContent
-function clickPark(){
-    card1.style.display = 'none';
-    card2.style.display = 'none';
-    card3.style.display = 'flex';
-    card4.style.display = 'none';
-    card5.style.display = 'none';
-    card6.style.display = 'none';
-    card7.style.display = 'none';
-    card8.style.display = 'none';
-}
-function clickPamyat(){
-    card1.style.display = 'flex';
-    card2.style.display = 'none';
-    card3.style.display = 'none';
-    card4.style.display = 'none';
-    card5.style.display = 'none';
-    card6.style.display = 'none';
-    card7.style.display = 'flex';
-    card8.style.display = 'none';
-}
-function clickMuseam(){
-    card1.style.display = 'none';
-    card2.style.display = 'none';
-    card3.style.display = 'none';
-    card4.style.display = 'none';
-    card5.style.display = 'none';
-    card6.style.display = 'none';
-    card7.style.display = 'none';
-    card8.style.display = 'flex';
-}
-function clickHram(){
-    card1.style.display = 'none';
-    card2.style.display = 'none';
-    card3.style.display = 'none';
-    card4.style.display = 'none';
-    card5.style.display = 'flex';
-    card6.style.display = 'none';
-    card7.style.display = 'none';
-    card8.style.display = 'none';
-}
-function clickPlace(){
-    card1.style.display = 'none';
-    card2.style.display = 'flex';
-    card3.style.display = 'none';
-    card4.style.display = 'flex';
-    card5.style.display = 'none';
-    card6.style.display = 'flex';
-    card7.style.display = 'none';
-    card8.style.display = 'none';
-}
-function Clear(){
-    card1.style.display = 'flex';
-    card2.style.display = 'flex';
-    card3.style.display = 'flex';
-    card4.style.display = 'flex';
-    card5.style.display = 'flex';
-    card6.style.display = 'flex';
-    card7.style.display = 'flex';
-    card8.style.display = 'flex';
-}
-document.getElementById('search').addEventListener('input', function() {
-    const searchTerm = this.value.toLowerCase();
-    const cards = document.querySelectorAll('.search__box');
-    cards.forEach(card => {
-        const title = card.querySelector('.search__title').textContent.toLowerCase();
-        if (title.includes(searchTerm)) {
-            card.style.display = 'flex';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-});
+const search = document.querySelectorAll('.search__sort_item')
+const sort = document.querySelectorAll('.sort')
 
 const itemsPerPage = 4;
 const items = document.querySelectorAll('.search__box');
@@ -121,9 +41,56 @@ document.getElementById('next').addEventListener('click', () => {
     }
 });
 
-const card = document.querySelector('.search__box');
-function moreInfo(){
-    card.addEventListener('click', function(){
-        card.classList.toggle('search__box-show')
-    })
+for(let i = 0; i < search.length; i++) {
+    search[i].addEventListener("click", function() {
+        if(search[i].textContent == 'Парки'){
+            sort.forEach(sort => {
+                sort.parentElement.parentElement.style.display = 'none'
+            })
+            document.querySelectorAll('.sort__parks').forEach(sort => sort.parentElement.parentElement.style.display = 'block')
+        }
+        if(search[i].textContent == 'Памятники'){
+            sort.forEach(sort => {
+                sort.parentElement.parentElement.style.display = 'none'
+            })
+            document.querySelectorAll('.sort__pamyat').forEach(sort => sort.parentElement.parentElement.style.display = 'block')
+        }
+        if(search[i].textContent == 'Музеи'){
+            sort.forEach(sort => {
+                sort.parentElement.parentElement.style.display = 'none'
+            })
+            document.querySelectorAll('.sort__museam').forEach(sort => sort.parentElement.parentElement.style.display = 'block')
+        }
+        if(search[i].textContent == 'Храмы'){
+            sort.forEach(sort => {
+                sort.parentElement.parentElement.style.display = 'none'
+            })
+            document.querySelectorAll('.sort__hram').forEach(sort => sort.parentElement.parentElement.style.display = 'block')
+        }
+        if(search[i].textContent == 'Исторические места'){
+            sort.forEach(sort => {
+                sort.parentElement.parentElement.style.display = 'none'
+            })
+            document.querySelectorAll('.sort__places').forEach(sort => sort.parentElement.parentElement.style.display = 'block')
+        }
+        if(search[i].textContent == 'Очистить'){
+            sort.forEach(sort => {
+                sort.parentElement.parentElement.style.display = 'block'
+                showPage(currentPage)
+            })
+        }
+    });
 }
+
+document.getElementById('search').addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase();
+    const cards = document.querySelectorAll('.search__box');
+    cards.forEach(card => {
+        const title = card.querySelector('.search__title').textContent.toLowerCase();
+        if (title.includes(searchTerm)) {
+            card.style.display = 'flex';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
