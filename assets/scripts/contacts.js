d869c8a4c9e695c3365888021e07d04d2c0b7e3d
Вадим Белкин <VaBelkin@alabuga.local>
2024-11-14 14:56:46 +0300

    modal update

--- assets/scripts/contacts.js
+++ assets/scripts/contacts.js
@@ -1,10 +1,11 @@
-const modal = document.querySelector('#modal')
-const body = document.querySelector('.body')
+const modal = document.querySelector('#modal');
+const body = document.querySelector('.body');
 function Close(){
-    modal.style.display = 'none'
-    body.style.overflow = 'visible'
-}
+    modal.style.display = 'none';
+    body.style.overflow = 'visible';
+};
 function openn(){
-    modal.style.display = 'block'
-    body.style.overflow = 'hidden'
-}
+    modal.style.display = 'block';
+    body.style.overflow = 'hidden';
+    modal.style['backdrop-filter'] = 'blur(5px)';
+};
