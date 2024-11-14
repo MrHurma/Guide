c55b75dc55fd13b91483a7fe665957b7de316e39
Вадим Белкин <VaBelkin@alabuga.local>
2024-11-14 13:46:06 +0300

    preloader update

--- assets/scripts/preloader.js
+++ assets/scripts/preloader.js
@@ -2,10 +2,12 @@ const boddy = document.querySelector('.body');
 document.addEventListener('DOMContentLoaded', function(){
     const preload = document.getElementById('main_preloader');
     window.onload = function(){
-        preload.style.opacity = '0';
         setTimeout(function(){
-            preload.style.display = 'none';
-            boddy.style['overflow-y'] = 'visible';
-        }, 700);
+            preload.style.opacity = '0';
+            setTimeout(function(){
+                preload.style.display = 'none';
+                boddy.style['overflow-y'] = 'visible';
+            }, 700);
+        }, 150);
     };
 });
