cba5302d33c3a8f1be628168335b94dda825b702
Вадим Белкин <VaBelkin@alabuga.local>
2024-11-19 15:27:02 +0300

    mini card update

--- assets/scripts/card.js
+++ assets/scripts/card.js
@@ -1,13 +1,12 @@
 const guide = 'https://672cae281600dda5a9f974a0.mockapi.io/Card/';
 let reques = new XMLHttpRequest();
-const cardMap = document.querySelector('.card__adress_box')
-cardMap.appendChild(document.createElement('div'))
 reques.open("GET", guide);
 reques.responseType = "json";
 
 reques.onload = () => {
     const get = textContent = reques.response;
-    let indx = 0;
+    let indx = 4;
+    console.log('wefwefwef');
     document.querySelector('.main').innerHTML = 
     `
     <div class="card" id="card">
@@ -21,7 +20,9 @@ reques.onload = () => {
                 <p class="card__text">${get[indx].text}</p>
                 <div class="card__adress_box">
                     <a href="" class="card__adress">${get[indx].address}</a>
-                    <div></div>
+                    <div style="position:relative;overflow:hidden;">
+                        <iframe src="${get[indx].map}" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,6 +32,3 @@ reques.onload = () => {
 
 reques.send();
 
-<div style="position:relative;overflow:hidden;">
-    <iframe src="${getItem.src_map}" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>
-</div>
